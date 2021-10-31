c92ed5418893c044273c52128e28dcd99aa84cf0
unryeong <leyng7@gmail.com>
2021-10-31 22:42:53 +0900

    feat: 서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -3,7 +3,6 @@ package hello.servlet.basic.response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
